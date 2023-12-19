ed24caeca3cc4c1f5d89a69e946e72020bb82d20
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-12-18 20:21:43 -0600

    update widgets and UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,167 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "installer.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Initialize Widgets
+    initialize_welcome();
+    ui->stack_installation->setCurrentIndex(0);
+
+    // Connect Widgets
+    connect(ui->btn_next, &QPushButton::clicked, this, &MainWindow::clicked_next);
+    connect(ui->btn_back, &QPushButton::clicked, this, &MainWindow::clicked_back);
+    connect(ui->checkbox_eula, &QCheckBox::stateChanged, this, &MainWindow::checked_eula);
+    connect(ui->line_lethalCompanyLocation, &QLineEdit::textChanged, this, &MainWindow::typed_gameLocation);
+    connect(ui->btn_browseLethalCompanyLocation, &QPushButton::clicked, this, &MainWindow::clicked_browse);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initialize_welcome() {
+    pageCompleted = true;
+    ui->btn_next->setEnabled(pageCompleted);
+    ui->btn_back->setEnabled(false);
+}
+
+void MainWindow::initialize_eula() {
+    pageCompleted = ui->checkbox_eula->isChecked();
+    ui->btn_next->setEnabled(pageCompleted);
+    ui->btn_back->setEnabled(true);
+}
+
+void MainWindow::initialize_configuration() {
+    pageCompleted = false;
+    ui->btn_next->setEnabled(pageCompleted);
+    ui->btn_back->setEnabled(true);
+
+    try {
+        // Find the game installation
+        installLocation = installer.findGameInstallation();
+        ui->line_lethalCompanyLocation->setText(QString(installLocation.c_str()));
+        ui->label_found->setText(QString("Yes"));
+
+        // Find the space available
+        std::string spaceAvailable = std::to_string(double(installer.getSpaceAvailable()/10000000000.0));
+        ui->label_spaceAvailable->setText(QString(spaceAvailable.c_str()));
+    } catch (GameNotFoundException e) {
+        ui->label_found->setText(QString("No"));
+    }
+}
+
+void MainWindow::initialize_working() {
+    pageCompleted = false;
+    ui->btn_next->setEnabled(pageCompleted);
+    ui->btn_back->setEnabled(false);
+}
+
+void MainWindow::initialize_done() {
+    pageCompleted = true;
+    ui->btn_next->setEnabled(false);
+    ui->btn_back->setEnabled(true);
+}
+
+void MainWindow::initialize_error() {
+    pageCompleted = true;
+    ui->btn_next->setEnabled(false);
+    ui->btn_back->setEnabled(false);
+}
+
+void MainWindow::clicked_next() {
+    int currentIndex = ui->stack_installation->currentIndex();
+    int nextIndex = currentIndex + 1;
+
+    if (nextIndex > ui->stack_installation->count()) {
+        return;
+    }
+
+    switch (nextIndex) {
+    case (0):
+        initialize_welcome();
+        break;
+    case (1):
+        initialize_eula();
+        break;
+    case (2):
+        initialize_configuration();
+        break;
+    case (3):
+        initialize_working();
+        break;
+    case (4):
+        initialize_done();
+        break;
+    case (5):
+        initialize_error();
+        break;
+    default:
+        initialize_welcome();
+        break;
+    }
+
+    ui->stack_installation->setCurrentIndex(nextIndex);
+}
+
+void MainWindow::clicked_back() {
+    int currentIndex = ui->stack_installation->currentIndex();
+    int previousIndex = currentIndex - 1;
+
+    if (previousIndex < 0) {
+        return;
+    }
+
+    switch (previousIndex) {
+    case (0):
+        initialize_welcome();
+        break;
+    case (1):
+        initialize_eula();
+        break;
+    case (2):
+        initialize_configuration();
+        break;
+    case (3):
+        initialize_working();
+        break;
+    case (4):
+        initialize_done();
+        break;
+    case (5):
+        initialize_error();
+        break;
+    default:
+        initialize_welcome();
+        break;
+    }
+    ui->stack_installation->setCurrentIndex(previousIndex);
+}
+
+void MainWindow::clicked_browse() {
+    QFileDialog::getExistingDirectory();
+}
+
+void MainWindow::checked_eula() {
+    pageCompleted = !pageCompleted;
+    ui->btn_next->setEnabled(pageCompleted);
+}
+
+void MainWindow::typed_gameLocation() {
+    // Check if the typed path exists
+    if (std::filesystem::exists(ui->line_lethalCompanyLocation->text().toStdString())) {
+        installLocation = ui->line_lethalCompanyLocation->text().toStdString();
+        ui->line_lethalCompanyLocation->setStyleSheet("border: 1px solid green");
+        // Find the space available
+        std::string spaceAvailable = std::to_string(double(installer.getSpaceAvailable()/10000000000.0));
+        ui->label_spaceAvailable->setText(QString(spaceAvailable.c_str()));
+        return;
+    }
+    ui->line_lethalCompanyLocation->setStyleSheet("border: 1px solid red");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "installer.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,29 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    //===== Page Initializations
+    void initialize_welcome();
+    void initialize_eula();
+    void initialize_configuration();
+    void initialize_working();
+    void initialize_done();
+    void initialize_error();
+
+    //===== Button Commands
+    void clicked_next();
+    void clicked_back();
+    void clicked_browse();
+
+    //===== Checkbox Commands
+    void checked_eula();
+
+    //===== Textbox Commands
+    void typed_gameLocation();
+
 private:
     Ui::MainWindow *ui;
+    bool pageCompleted;
+    std::string installLocation;
+    Installer installer;
 };
 #endif // MAINWINDOW_H
