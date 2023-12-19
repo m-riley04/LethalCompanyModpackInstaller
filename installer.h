61c43d9bb158524f7f34e60779426e701979e544
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-12-18 20:23:41 -0600

    add methods and variables

--- installer.h
+++ installer.h
@@ -2,6 +2,11 @@
 #define INSTALLER_H
 
 #include <string>
+class GameNotFoundException : public std::exception
+{
+public:
+    const char * what() const noexcept override;
+};
 
 class Installer
 {
@@ -14,18 +19,26 @@ public:
     void uninstall();
     void enable();
     void disable();
+    std::string findGameInstallation();
 
     //=== STATUS
     bool isUpdated();
 
     //=== GETTERS
     std::string getVersion();
+    std::string getGameLocation();
+    std::string getGameDrive();
+    int getInstallSize();
+    int getSpaceAvailable();
 
     //=== SETTERS
     void setVersion(std::string version);
 
 private:
+    int installSize;
     std::string version;
+    std::string gameLocation;
+    std::string gameDrive;
 };
 
 #endif // INSTALLER_H
