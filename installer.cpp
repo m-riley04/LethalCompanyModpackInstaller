1f7a28667af5f2da9e8a884fa0793426c97b9143
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-12-20 23:12:56 -0600

    Update installer class to focus on installing only

--- installer.cpp
+++ installer.cpp
@@ -2,24 +2,10 @@
 #include <filesystem>
 #include <QDebug>
 
-//=== EXCEPTIONS
-const char * GameNotFoundException::what() const noexcept {
-    return "The game not found.";
-}
-
 //=== CONSTRUCTORS
 Installer::Installer() {}
 
 //=== FUNCTIONALITIES
-// Updates the modpack
-void Installer::update() {
-    // Delete the current modpack files
-
-    // Download the latest version of the modpack
-
-    // Install the latest version of the modpack
-
-}
 // Installs the modpack
 void Installer::install() {
     // Locate the installation location for Lethal Company
@@ -33,71 +19,6 @@ void Installer::uninstall() {
     // Remove the BepInEx folder
 
 }
-// Enables the modpack
-void Installer::enable() {
-
-}
-// Disables the modpack
-void Installer::disable() {
-
-}
-
-// Finds the game's installation directory
-std::string Installer::findGameInstallation() {
-    // Check common drives
-    std::string commonDrives[10] = {"C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
-    std::filesystem::path gamePath;
-    std::string gameName = "Lethal Company";
-
-    // Iterate through each drive
-    for (auto & drive : commonDrives) {
-
-        std::string pathStr = drive;
-        pathStr += ":\\SteamLibrary\\steamapps\\common";
-        gamePath /= pathStr;
-        gamePath /= gameName;
-
-        qDebug() << gamePath.string();
-
-        // Check if the Steam game exists
-        if (std::filesystem::exists(gamePath)) {
-            gameLocation = gamePath.string();
-            gameDrive = drive;
-            gameDrive += ":\\";
-            return gamePath.string();
-        }
-
-        // Otherwise, try again
-        gamePath.clear();
-    }
-
-    throw GameNotFoundException();
-}
-
-//=== STATUS
-// Returns whether the modpack is updated to the latest release or not
-bool Installer::isUpdated() {
-    // Get the latest version number
-    std::string latestVersion;
-    // TODO - Get latest version number
-
-    // Compare it to the current version
-    if (latestVersion != this->version) { return false; }
-    return true;
-
-}
-
-//=== GETTERS
-// Returns the current version
-std::string Installer::getVersion() { return this->version; }
-
-int Installer::getSpaceAvailable() {
-    return std::filesystem::space(std::filesystem::path(gameDrive)).available;
-}
-
-//=== SETTERS
-// Sets the current version
-void Installer::setVersion(std::string version) { this->version = version; }
 
 
 
--- installer.h
+++ installer.h
@@ -2,11 +2,6 @@
 #define INSTALLER_H
 
 #include <string>
-class GameNotFoundException : public std::exception
-{
-public:
-    const char * what() const noexcept override;
-};
 
 class Installer
 {
@@ -14,31 +9,17 @@ public:
     Installer();
 
     //=== FUNCTIONALITIES
-    void update();
     void install();
     void uninstall();
-    void enable();
-    void disable();
-    std::string findGameInstallation();
-
-    //=== STATUS
-    bool isUpdated();
 
     //=== GETTERS
-    std::string getVersion();
+    int getInstallSize();
     std::string getGameLocation();
     std::string getGameDrive();
-    int getInstallSize();
     int getSpaceAvailable();
-
-    //=== SETTERS
-    void setVersion(std::string version);
 
 private:
     int installSize;
-    std::string version;
-    std::string gameLocation;
-    std::string gameDrive;
 };
 
 #endif // INSTALLER_H
